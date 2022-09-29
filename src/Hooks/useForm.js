83e2325458ea26bca532ff9a620b3a477279af52
Joao Pedro Correia <jpedrocbarros@gmail.com>
2022-09-28 21:37:40 -0300

    email regex

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+const validacao = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+};
+
 const useForm = () => {
   const [value, setValue] = useState("");
 
