e2c4e349d6dab904f315cfae7f58c8bb69504034
Joao Pedro Correia <jpedrocbarros@gmail.com>
2022-09-28 21:33:08 -0300

    useForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
   function handleSubmit(event) {
     event.preventDefault();
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
@@ -26,8 +30,8 @@ const LoginForm = () => {
     <div>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,5 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useForm = () => {
   const [value, setValue] = useState("");
+
+  function onChange({ target }) {
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+  };
 };
+
+export default useForm;
