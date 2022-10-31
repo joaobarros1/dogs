7ccc64b7a3880a5088858539dc905fab237c1b5e
Joao Pedro Correia <jpedrocbarros@gmail.com>
2022-10-30 23:36:34 -0300

    title

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserHeaderNav from "./UserHeaderNav";
 import styles from "./UserHeader.module.css";
+import { useLocation } from "react-router-dom";
 
 const UserHeader = () => {
+  const [title, setTitle] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const { pathname } = location;
+    switch (pathname) {
+      case "/conta/postar":
+        setTitle("Poste sua foto");
+        break;
+      case "/conta/estatisticas":
+        setTitle("Estatisticas");
+        break;
+      default:
+        setTitle("Minha Conta");
+    }
+  }, [location]);
+
   return (
     <header className={styles.header}>
-      <h1 className="title">Titulo</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
