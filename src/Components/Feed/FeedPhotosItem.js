6583ebca339f807dd45856cab43da26a0b20476d
João Pedro Correia <jpedrocbarros@gmail.com>
2022-11-01 16:29:02 -0300

    modal 1

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
 
-const FeedPhotosItem = ({ photo }) => {
-  if (photo) {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
+  if (photo)
     return (
-      <li className={styles.photo}>
+      <li className={styles.photo} onClick={handleClick}>
         <img src={photo.src} alt={photo.title} />
         <span className={styles.visualizacao}>{photo.acessos}</span>
       </li>
     );
-  }
 };
 
 export default FeedPhotosItem;
